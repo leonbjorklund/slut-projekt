3622efbc7b701ec6210781bcca811d5bbf1477c5
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-05-19 16:03:10 +0200

    add loginUser functionality

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import { UserModel } from "../models/user-model";
 
@@ -6,6 +7,24 @@ export async function createUser(req: Request, res: Response) {
   await user.save();
   console.log(req.body);
   return res.status(201).json(user);
+}
+
+export async function loginUser(req: Request, res: Response) {
+  const { email, password } = req.body;
+
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  return res.status(200).json({ message: "Login successful" });
 }
 
 export async function getAllUsers(req: Request, res: Response) {
--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -3,12 +3,14 @@ import {
   createUser,
   getAllUsers,
   getUserById,
+  loginUser,
   updateUser,
 } from "../controllers/user-controller";
 
 export const userRouter = Router();
 
 userRouter.post("/api/users", createUser);
+userRouter.post("/api/users/login", loginUser);
 userRouter.get("/api/users", getAllUsers);
 userRouter.get("/api/users/:id", getUserById);
 userRouter.put("/api/users/:id", updateUser);
