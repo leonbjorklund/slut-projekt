f36e278c15293f75a0f127a542f646d1684be173
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-05-19 15:48:48 +0200

    add createUser-functionality

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,17 +1,21 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import { UserModel } from "../models/user-model";
 
 export async function createUser(req: Request, res: Response) {
-  res.send('createUser');
+  const user = await UserModel.create(req.body);
+  await user.save();
+  console.log(req.body);
+  return res.status(201).json(user);
 }
 
 export async function getAllUsers(req: Request, res: Response) {
-  res.send('getAllUsers');
+  res.send("getAllUsers");
 }
 
 export async function getUserById(req: Request, res: Response) {
-  res.send('getUserById');
+  res.send("getUserById");
 }
 
 export async function updateUser(req: Request, res: Response) {
-  res.send('updateUser');
+  res.send("updateUser");
 }
