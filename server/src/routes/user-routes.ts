e64d9b8774365bede8961867e559622033227ad5
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-05-19 15:51:43 +0200

    add user-routes

--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -1,16 +1,16 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
   createUser,
   getAllUsers,
   getUserById,
   updateUser,
-} from '../controllers/user-controller';
+} from "../controllers/user-controller";
 
-const userRouter = Router();
+export const userRouter = Router();
 
-userRouter.post('/api/users', createUser);
-userRouter.get('/api/users', getAllUsers);
-userRouter.get('/api/users/:id', getUserById);
-userRouter.put('/api/users/:id', updateUser);
+userRouter.post("/api/users", createUser);
+userRouter.get("/api/users", getAllUsers);
+userRouter.get("/api/users/:id", getUserById);
+userRouter.put("/api/users/:id", updateUser);
 
 export default userRouter;
