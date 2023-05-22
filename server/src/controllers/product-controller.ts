c92dfe5fc7e02c46aeb28c11dc119d4662c28baf
Jennifer Techel <jennifertechel@hotmail.com>
2023-05-22 12:13:53 +0200

    add product

--- server/src/controllers/product-controller.ts
+++ server/src/controllers/product-controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
+import { Product, ProductModel } from "../models/product-model";
 
 export async function addProduct(req: Request, res: Response) {
-  res.send("createUser");
+  try {
+    const newProduct: Product = req.body;
+    const product = await ProductModel.create(newProduct);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to add product" });
+  }
 }
 
 export async function getAllProducts(req: Request, res: Response) {
-  res.send("getAllUsers");
+  try {
+    const products: Product[] = await ProductModel.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to get products" });
+  }
 }
 
 export async function getProductById(req: Request, res: Response) {
