af8aeff0d3e4ad81a39c766f36ced40958b2655c
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-05-22 10:26:14 +0200

    add cookie session

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,4 @@
+import cookieSession from "cookie-session";
 import express from "express";
 import { errorHandler } from "./errorHandler";
 import userRouter from "./routes/user-routes";
@@ -5,6 +6,16 @@ import userRouter from "./routes/user-routes";
 export const app = express();
 
 app.use(express.json());
+
+app.use(
+  cookieSession({
+    name: "session",
+    secret: "asdkhl847sglkj374hj39sglk5j7",
+    maxAge: 1 * 60 * 60 * 1000, // 1 hour
+    secure: false,
+    httpOnly: true,
+  })
+);
 
 app.use(userRouter);
 
