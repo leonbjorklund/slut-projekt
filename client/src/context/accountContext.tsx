a8cf9fa5663b07870796f8920ccf0dac0e0fa987
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-05-22 15:25:28 +0200

    update context with register, login and logout

--- client/src/context/accountContext.tsx
+++ client/src/context/accountContext.tsx
@@ -1,17 +1,23 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 interface User {
   email: string;
   password: string;
+  isAdmin: boolean;
 }
 
-const AccountContext = createContext<{
+interface AccountContextType {
   user: User | null;
-  setUser: React.Dispatch<React.SetStateAction<User | null>>;
-}>({
+  register: (username: string, password: string) => Promise<void>;
+  login: (username: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+const AccountContext = createContext<AccountContextType>({
   user: null,
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  setUser: () => {},
+  register: async () => {},
+  login: async () => {},
+  logout: async () => {},
 });
 
 interface AccountProviderProps {
@@ -22,9 +28,66 @@ export const AccountProvider: React.FC<AccountProviderProps> = ({
   children,
 }) => {
   const [user, setUser] = useState<User | null>(null);
+  const [users, setUsers] = useState<User[] | null>(null);
+  const { user: loggedInUser } = useAccount();
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      const response = await fetch("/api/users/auth", {
+        method: "GET",
+        credentials: "include",
+      });
+      if (response.ok) {
+        const userFromSession = await response.json();
+        setUser(userFromSession);
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
+
+  const register = async (email: string, password: string) => {
+    const response = await fetch("/api/users/", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    if (response.ok) {
+      const user = await response.json();
+      setUser(user);
+    }
+  };
+
+  const login = async (email: string, password: string) => {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    if (response.ok) {
+      const user = await response.json();
+      setUser(user);
+    }
+  };
+
+  const logout = async () => {
+    const response = await fetch("/api/users/signout", {
+      method: "POST",
+      credentials: "include",
+    });
+    if (response.ok) {
+      setUser(null);
+    }
+  };
 
   return (
-    <AccountContext.Provider value={{ user, setUser }}>
+    <AccountContext.Provider value={{ user, register, login, logout }}>
       {children}
     </AccountContext.Provider>
   );
