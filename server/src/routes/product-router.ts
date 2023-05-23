a88427d0c0b05d9cc8768e363836d04b60015a87
Jennifer Techel <jennifertechel@hotmail.com>
2023-05-23 11:13:12 +0200

    correct deleteProduct import

--- server/src/routes/product-router.ts
+++ server/src/routes/product-router.ts
@@ -4,8 +4,8 @@ import {
   getAllProducts,
   getProductById,
   updateProduct,
+  deleteProduct,
 } from "../controllers/product-controller";
-import { deleteModel } from "mongoose";
 
 const productRouter = Router();
 
@@ -13,6 +13,6 @@ productRouter.post("/api/products", addProduct);
 productRouter.get("/api/products", getAllProducts);
 productRouter.get("/api/products/:id", getProductById);
 productRouter.put("/api/products/:id", updateProduct);
-productRouter.put("/api/products/:id", deleteModel);
+productRouter.delete("/api/products/:id", deleteProduct);
 
 export default productRouter;
