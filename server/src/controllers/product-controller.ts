7777207f3ffe4d08ddf1484845753ca269bfa1f8
Jennifer Techel <jennifertechel@hotmail.com>
2023-05-23 11:13:31 +0200

    all functions for product working

--- server/src/controllers/product-controller.ts
+++ server/src/controllers/product-controller.ts
@@ -21,13 +21,42 @@ export async function getAllProducts(req: Request, res: Response) {
 }
 
 export async function getProductById(req: Request, res: Response) {
-  res.send("getUserById");
+  try {
+    const productId = req.params.id;
+    await ProductModel.findById(productId);
+    res.status(200).json({ message: "Product successfully found" });
+  } catch (error) {
+    res.status(500).json({ error: "Product not found " });
+  }
 }
 
 export async function updateProduct(req: Request, res: Response) {
-  res.send("updateUser");
+  try {
+    const productId = req.params.id;
+    const updatedProductData: Product = req.body;
+
+    const updatedProduct = await ProductModel.findByIdAndUpdate(
+      productId,
+      updatedProductData,
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update product" });
+  }
 }
 
 export async function deleteProduct(req: Request, res: Response) {
-  res.send("updateUser");
+  try {
+    const productId = req.params.id;
+    await ProductModel.findByIdAndDelete(productId);
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete product" });
+  }
 }
