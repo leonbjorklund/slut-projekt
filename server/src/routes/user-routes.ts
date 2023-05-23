06f21636ecb9027bfa4b7f2da64b14f7edbb352a
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-05-23 10:29:42 +0200

    change order of routes

--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -13,10 +13,10 @@ export const userRouter = Router();
 
 userRouter.post("/api/users/create", createUser);
 userRouter.post("/api/users/login", loginUser);
+userRouter.get("/api/users/auth", checkAuth);
 userRouter.post("/api/users/signout", signoutUser);
 userRouter.put("/api/users/:id", updateUserAdmin);
 userRouter.get("/api/users", getAllUsers);
 userRouter.get("/api/users/:id", getUserById);
-userRouter.get("/api/users/auth", checkAuth);
 
 export default userRouter;
