fc9616d27bb363a7572f505af12d41a1f843243e
Jennifer Techel <jennifertechel@hotmail.com>
2023-05-23 13:41:05 +0200

    add functions for file upload

---
+++ server/src/models/file-model.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export let fileBucket: mongoose.mongo.GridFSBucket;
+
+mongoose.connection.on("open", () => {
+  fileBucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+    bucketName: "files",
+  });
+});
