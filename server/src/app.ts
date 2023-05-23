acb0f562bea345d2042b20b311b215196d80966e
MadeleineGustafson <madeleinegu@gmail.com>
2023-05-23 12:17:15 +0200

    merge main

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import "express-async-errors";
 import { cookieSessionMiddleware } from "./cookieSession";
 import { errorHandler } from "./errorHandler";
 import orderRouter from "./routes/order-routes";
+import productRouter from "./routes/product-router";
 import userRouter from "./routes/user-routes";
 
 export const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieSessionMiddleware);
 
 app.use(userRouter);
+app.use(productRouter);
 app.use(orderRouter);
 
 app.use(errorHandler);
