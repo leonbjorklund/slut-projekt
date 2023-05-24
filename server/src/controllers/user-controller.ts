cad0d82844bea95b2f15a225565f1ddf7600a836
Leon Björklund <leon.bjorklund@gmail.com>
2023-05-24 11:00:19 +0200

    disabled multiple users signup

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -4,8 +4,17 @@ import { assert } from "../errorHandler";
 import { UserModel } from "../models/user-model";
 
 export async function createUser(req: Request, res: Response) {
+  const { email } = req.body;
+
+  // check if user with given email already exists
+  const existingUser = await UserModel.findOne({ email });
+
+  // if user already exists, throw an error
+  assert(existingUser === null, 409, "User already exists!");
+
   const user = await UserModel.create(req.body);
   await user.save();
+
   return res.status(201).json(user);
 }
 
