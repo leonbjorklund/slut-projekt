cdadd2ecde106963a24e45847a25f09affdbeda3
Leon Björklund <leon.bjorklund@gmail.com>
2023-05-26 15:55:35 +0200

    set timeout

--- server/tests/user-registration.g.test.ts
+++ server/tests/user-registration.g.test.ts
@@ -13,90 +13,106 @@ describe("Registering a user (POST)", () => {
     db = await mockDB();
   });
 
-  it("should be possible to register a user (200)", async () => {
-    const response = await request(app)
-      .post("/api/users/create")
-      .set("content-type", "application/json")
-      .send({ email: "new-user@plugga.se", password: "123123" });
-
-    // Assert response is correct
-    expect(response.status).toBe(201);
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body._id).toBeDefined();
-    expect(response.body.email).toBe("new-user@plugga.se");
-    expect(response.body.password).toBeUndefined();
-    expect(response.body.isAdmin).toBe(false);
-  });
-
-  it("should not be possible to register a user with an existsing email (409)", async () => {
-    const response = await request(app)
-      .post("/api/users/create")
-      .set("content-type", "application/json")
-      .send({ email: "user@plugga.se", password: "123123" });
-
-    // Assert response is correct
-    expect(response.status).toBe(409);
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(typeof response.body).toBe("string");
-  });
-
-  it("should not be possible to register a user with incorrect or missing values (400)", async () => {
-    const user = { email: "new-user@plugga.se", password: "123123" };
-
-    // Incorrect values
-    for (const key of Object.keys(user)) {
-      const faultyUser = { ...user, [key]: false };
-
+  it(
+    "should be possible to register a user (200)",
+    async () => {
       const response = await request(app)
         .post("/api/users/create")
         .set("content-type", "application/json")
-        .send(faultyUser);
+        .send({ email: "new-user@plugga.se", password: "123123" });
 
       // Assert response is correct
-      expect(response.status).toBe(400);
+      expect(response.status).toBe(201);
       expect(response.headers["content-type"]).toMatch(/json/);
-      expect(response.body).toMatch(new RegExp(`${key}`, "i"));
-    }
+      expect(response.body._id).toBeDefined();
+      expect(response.body.email).toBe("new-user@plugga.se");
+      expect(response.body.password).toBeUndefined();
+      expect(response.body.isAdmin).toBe(false);
+    },
+    { timeout: 20000 }
+  );
 
-    // Missing values
-    for (const key of Object.keys(user)) {
-      const faultyUser: any = { ...user };
-      delete faultyUser[key];
-
+  it(
+    "should not be possible to register a user with an existsing email (409)",
+    async () => {
       const response = await request(app)
         .post("/api/users/create")
         .set("content-type", "application/json")
-        .send(faultyUser);
+        .send({ email: "user@plugga.se", password: "123123" });
 
       // Assert response is correct
-      expect(response.status).toBe(400);
+      expect(response.status).toBe(409);
       expect(response.headers["content-type"]).toMatch(/json/);
-      expect(response.body).toMatch(new RegExp(`${key}`, "i"));
-    }
-  });
+      expect(typeof response.body).toBe("string");
+    },
+    { timeout: 20000 }
+  );
 
-  it("should encrypt users password with salt when registering a new user", async () => {
-    await request(app)
-      .post("/api/users/create")
-      .set("content-type", "application/json")
-      .send({ email: "user1@plugga.se", password: "s3cr3t" });
-    await request(app)
-      .post("/api/users/create")
-      .set("content-type", "application/json")
-      .send({ email: "user2@plugga.se", password: "s3cr3t" });
+  it(
+    "should not be possible to register a user with incorrect or missing values (400)",
+    async () => {
+      const user = { email: "new-user@plugga.se", password: "123123" };
 
-    const user1 = await UserModel.findOne({
-      email: "user1@plugga.se",
-    }).select("+password");
-    // const argon2Match =
-    //   user1 && (await argon2.verify(user1.password, 's3cr3t'));
-    const bcryptMatch =
-      user1 && (await bcrypt.compare("s3cr3t", user1.password));
-    expect(bcryptMatch).to.be.true;
+      // Incorrect values
+      for (const key of Object.keys(user)) {
+        const faultyUser = { ...user, [key]: false };
 
-    const user2 = await UserModel.findOne({
-      email: "user2@plugga.se",
-    }).select("+password");
-    expect(user2?.password).not.toEqual(user1?.password);
-  });
+        const response = await request(app)
+          .post("/api/users/create")
+          .set("content-type", "application/json")
+          .send(faultyUser);
+
+        // Assert response is correct
+        expect(response.status).toBe(400);
+        expect(response.headers["content-type"]).toMatch(/json/);
+        expect(response.body).toMatch(new RegExp(`${key}`, "i"));
+      }
+
+      // Missing values
+      for (const key of Object.keys(user)) {
+        const faultyUser: any = { ...user };
+        delete faultyUser[key];
+
+        const response = await request(app)
+          .post("/api/users/create")
+          .set("content-type", "application/json")
+          .send(faultyUser);
+
+        // Assert response is correct
+        expect(response.status).toBe(400);
+        expect(response.headers["content-type"]).toMatch(/json/);
+        expect(response.body).toMatch(new RegExp(`${key}`, "i"));
+      }
+    },
+    { timeout: 20000 }
+  );
+
+  it(
+    "should encrypt users password with salt when registering a new user",
+    async () => {
+      await request(app)
+        .post("/api/users/create")
+        .set("content-type", "application/json")
+        .send({ email: "user1@plugga.se", password: "s3cr3t" });
+      await request(app)
+        .post("/api/users/create")
+        .set("content-type", "application/json")
+        .send({ email: "user2@plugga.se", password: "s3cr3t" });
+
+      const user1 = await UserModel.findOne({
+        email: "user1@plugga.se",
+      }).select("+password");
+      // const argon2Match =
+      //   user1 && (await argon2.verify(user1.password, 's3cr3t'));
+      const bcryptMatch =
+        user1 && (await bcrypt.compare("s3cr3t", user1.password));
+      expect(bcryptMatch).to.be.true;
+
+      const user2 = await UserModel.findOne({
+        email: "user2@plugga.se",
+      }).select("+password");
+      expect(user2?.password).not.toEqual(user1?.password);
+    },
+    { timeout: 20000 }
+  );
 });
