ff8a84f8620eeb6550768518320a43b880d7a075
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-05-29 15:20:28 +0200

    update route

--- server/src/routes/order-routes.ts
+++ server/src/routes/order-routes.ts
@@ -11,8 +11,8 @@ const orderRouter = Router();
 
 orderRouter.post("/api/orders", createOrder);
 orderRouter.get("/api/orders", getAllOrders);
+orderRouter.get("/api/orders/:email", getOrdersByUser);
 orderRouter.get("/api/orders/:id", getOrderById);
 orderRouter.put("/api/orders/:id", updateShippingStatus);
-orderRouter.get("/api/orders/user/:email", getOrdersByUser);
 
 export default orderRouter;
