f1572ac5ba2360a1f880f6b329564d6ca626e9da
MadeleineGustafson <madeleinegu@gmail.com>
2023-05-29 13:20:29 +0200

    commented out

--- client/src/pages/OrderConfirmation.tsx
+++ client/src/pages/OrderConfirmation.tsx
@@ -1,5 +1,4 @@
-import { Box, Center, Flex, Heading, Text } from "@chakra-ui/react";
-import OrderCard from "../components/OrderCard";
+import { Box, Center, Heading, Text } from "@chakra-ui/react";
 import { useOrder } from "../context/orderContext";
 
 function OrderConfirmation() {
@@ -10,22 +9,22 @@ function OrderConfirmation() {
       <Box py={8}>
         <Heading fontSize='1.5rem'>Thank you for your order! </Heading>
       </Box>
-      <Box
+      {/* <Box
         w='40%'
         borderBottom='1px'
         borderColor='blackAlpha.200'
         pt={6}
         pb={2}
-      >
-        <Text fontSize='1.1rem'>Order number: {order?.id}</Text>
-      </Box>
+      > */}
+      {/* <Text fontSize='1.1rem'>Order number: {order?.id}</Text> */}
+      {/* </Box>
       {order?.cart.map((cartItem) => (
         <Box key={cartItem._id} w='100%'>
           <Center>
             <OrderCard cart={[cartItem]} />
           </Center>
         </Box>
-      ))}
+      ))} */}
       <Box w='40%' textAlign='right'>
         <Text fontSize='1.1rem'>Totalt: {order?.totalPrice} SEK</Text>
       </Box>
@@ -41,15 +40,15 @@ function OrderConfirmation() {
         <Text fontSize='1rem' fontWeight={800} textTransform='uppercase' pb={4}>
           Your information:
         </Text>
-        <Flex gap='0.3rem'>
-          <Text>{order?.formData.firstName}</Text>
+        {/* <Flex gap='0.3rem'> */}
+        {/* <Text>{order?.formData.firstName}</Text>
           <Text>{order?.formData.lastName}</Text>
         </Flex>
         <Text>{order?.formData.address}</Text>
         <Text>{order?.formData.zipcode}</Text>
         <Text>{order?.formData.city}</Text>
         <Text pt={2}>{order?.formData.email}</Text>
-        <Text>{order?.formData.phone}</Text>
+        <Text>{order?.formData.phone}</Text> */}
       </Box>
     </Center>
   );
