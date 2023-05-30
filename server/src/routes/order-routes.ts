33d03ea2260c6f9de24cab37b7da63537d21e22a
MadeleineGustafson <madeleinegu@gmail.com>
2023-05-30 13:30:20 +0200

    isShipped vlidation

--- server/src/routes/order-routes.ts
+++ server/src/routes/order-routes.ts
@@ -5,13 +5,20 @@ import {
   getOrderById,
   updateShippingStatus,
 } from "../controllers/order-controller";
-import { validateCreateOrder } from "../validation/order-validation";
+import {
+  validateCreateOrder,
+  validateUpdateShippingStatus,
+} from "../validation/order-validation";
 
 const orderRouter = Router();
 
 orderRouter.post("/api/orders", validateCreateOrder, createOrder);
 orderRouter.get("/api/orders", getAllOrders);
 orderRouter.get("/api/orders/:id", getOrderById);
-orderRouter.put("/api/orders/:id", updateShippingStatus);
+orderRouter.put(
+  "/api/orders/:id",
+  validateUpdateShippingStatus,
+  updateShippingStatus
+);
 
 export default orderRouter;
--- server/src/validation/order-validation.ts
+++ server/src/validation/order-validation.ts
@@ -5,9 +5,9 @@ const addressSchema = z.object({
   firstName: z.string().min(1).max(100),
   lastName: z.string().min(1).max(100),
   address: z.string(),
-  zipCode: z.number().default(0),
+  zipCode: z.number(),
   city: z.string(),
-  phoneNumber: z.number().default(0),
+  phoneNumber: z.number(),
 });
 
 const orderItemSchema = z.object({
@@ -37,6 +37,27 @@ export function validateCreateOrder(
   } else {
     const errorMessages = validationResult.error.errors.map(
       (error) => `Field '${error.path.join(".")}' is required`
+    );
+    res.status(400).json(errorMessages);
+  }
+}
+
+const updateShippingStatusSchema = z.object({
+  isShipped: z.boolean(),
+});
+
+export function validateUpdateShippingStatus(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const validationResult = updateShippingStatusSchema.safeParse(req.body);
+  if (validationResult.success) {
+    req.body = validationResult.data; // Update the request body with the validated data
+    next();
+  } else {
+    const errorMessages = validationResult.error.errors.map(
+      (error) => `Invalid value for '${error.path.join(".")}'`
     );
     res.status(400).json(errorMessages);
   }
