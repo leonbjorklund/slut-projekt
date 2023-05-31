467dec3dff1d60a7dbc4cea1d76aae889efbecf8
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-05-31 13:35:45 +0200

    add isOrderOwner middleware

--- server/src/routes/order-routes.ts
+++ server/src/routes/order-routes.ts
@@ -7,12 +7,13 @@ import {
   updateShippingStatus,
 } from "../controllers/order-controller";
 import { isAdmin } from "../middlewares/isAdmin";
+import { isOrderOwner } from "../middlewares/isOrderOwner";
 
 const orderRouter = Router();
 
 orderRouter.post("/api/orders", createOrder);
 orderRouter.get("/api/orders", isAdmin, getAllOrders);
-orderRouter.get("/api/orders/:email", getOrdersByUser);
+orderRouter.get("/api/orders/:email", isOrderOwner, getOrdersByUser);
 orderRouter.get("/api/orders/:id", getOrderById);
 orderRouter.put("/api/orders/:id", isAdmin, updateShippingStatus);
 
