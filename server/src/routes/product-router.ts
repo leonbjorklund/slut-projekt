be71033efb078d62f571e5e35cd101d9430d1593
Leon Björklund <leon.bjorklund@gmail.com>
2023-05-31 16:33:04 +0200

    test

--- server/src/routes/product-router.ts
+++ server/src/routes/product-router.ts
@@ -1,19 +1,27 @@
 import { Router } from "express";
 import {
   addProduct,
+  deleteProduct,
   getAllProducts,
   getProductById,
   updateProduct,
-  deleteProduct,
 } from "../controllers/product-controller";
-import { productCreateSchema, productEditSchema, validateBody } from "../validation/product-client-validation";
+import {
+  productCreateSchema,
+  validateBody,
+} from "../validation/product-client-validation";
 
 const productRouter = Router();
 
-productRouter.post("/api/products", validateBody(productCreateSchema), addProduct);
+productRouter.post(
+  "/api/products",
+  validateBody(productCreateSchema),
+  addProduct
+);
 productRouter.get("/api/products", getAllProducts);
 productRouter.get("/api/products/:id", getProductById);
-productRouter.put("/api/products/:id", validateBody(productEditSchema), updateProduct);
+// productRouter.put("/api/products/:id", validateBody(productEditSchema), updateProduct);
+productRouter.put("/api/products/:id", updateProduct);
 productRouter.delete("/api/products/:id", deleteProduct);
 
 export default productRouter;
