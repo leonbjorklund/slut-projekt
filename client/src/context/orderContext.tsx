41e0671a6623628a729853d4cccdb7293a25d669
Jennifer Techel <jennifertechel@hotmail.com>
2023-06-01 10:16:28 +0200

    remove localhost:3000

--- client/src/context/orderContext.tsx
+++ client/src/context/orderContext.tsx
@@ -83,7 +83,7 @@ export default function OrderProvider(props: PropsWithChildren<any>) {
     };
 
     try {
-      const response = await fetch("http://localhost:3000/api/orders", {
+      const response = await fetch("/api/orders", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
